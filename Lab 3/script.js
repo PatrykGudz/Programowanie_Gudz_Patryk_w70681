cd0c974704de31727f4aa6eb72643e46459c2a15
PatrykGudz <166228782+PatrykGudz@users.noreply.github.com>
2024-05-31 08:41:51 +0200

    Lab 3

---
+++ Lab 3/script.js
@@ -0,0 +1,66 @@
+//zad 2
+// alert("Hello World!");
+
+//zad 3 
+// let a = 10;
+// let b = 20;
+// let c = 23.2;
+// alert(a+b+c);
+// alert(c-b);
+// alert(b*c);
+// alert(b/a);
+// console.log(a+b);
+//let wynik = a+b+c;
+//document.getElementById("zad3").innerHTML="wynik dodawania: "+ wynik;
+
+//zad4
+// for(let i =0; i<101; i++){
+//     if(i%2==0){
+//         console.log(i)
+//     }
+// }
+
+//zad5
+// let a = parseFloat(prompt("Podaj dlugość a: "));
+// let b = parseFloat(prompt("Podaj dlugość b: "));
+// let c = parseFloat(prompt("Podaj dlugość c: "));
+
+// let p = (a+b+c)/2;
+// let Pol = Math.sqrt(p*(p-a)*(p-b)*(p-c));
+// console.log(Pol);
+// document.getElementById("pole").innerHTML="Pole wynosi: " + Pol;
+
+//zad6 
+// let imie = prompt("Podaj imie!");
+// alert("Witaj " +imie + "!");
+
+//zad7 
+// let a = parseInt(prompt("Podaj pierwszą liczbę całkowitą: "));
+// let b = parseInt(prompt("Podaj drugą liczbę całkowitą: ")); 
+
+// wynik = a+b;
+// alert(wynik)
+
+//zad8 
+// let a = parseFloat(prompt("Podaj a: "));
+// let b = parseFloat(prompt("Podaj b: "));
+// let c = parseFloat(prompt("Podaj c: "));
+
+// let najw = Math.max(a,b,c);
+
+// console.log(najw);
+//document.getElementById("zad8").innerHTML = "Największa liczba to: " + najw;
+
+
+//zad9 
+// let a = parseInt(prompt("Podaj a: "));
+// let b = parseInt(prompt("Podaj b: "));
+
+// let reszta
+
+// while(b){
+//     reszta = a%b;
+//     a=b;
+//     b=reszta;
+// }
+// alert(a)
